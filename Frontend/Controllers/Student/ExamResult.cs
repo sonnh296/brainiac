95d841407ce9e10494a20a0c58553c3aa9f3d040
tronghoa <hoa.truongtrong26@gmail.com>
2024-02-21 14:14:18 +0700

    feat(Student): add UI related to Taking Exam

---
+++ Frontend/Controllers/Student/ExamResult.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Frontend.Controllers.Student
+{
+    public class ExamResult : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Views/Student/ExamResult.cshtml");
+        }
+    }
+}
