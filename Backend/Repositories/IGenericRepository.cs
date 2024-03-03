d132b3f32417bebbb0da1a2c880d2f85f543cd4c
tronghoa <hoa.truongtrong26@gmail.com>
2024-03-03 11:43:26 +0700

    feat: add layer repository

---
+++ Backend/Repositories/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(object id);
+        void Add(T obj);
+        void Update(T obj);
+        void Delete(object id);
+        Task<T> GetByIdAsync();
+    }
+}
