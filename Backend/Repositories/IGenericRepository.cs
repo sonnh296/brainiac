717078f8cfc5148949cfa1bf2b84b503973693cc
cuongjoe <105358697+cuongjoe@users.noreply.github.com>
2024-03-04 13:41:13 +0700

    api manager user

--- Backend/Repositories/IGenericRepository.cs
+++ Backend/Repositories/IGenericRepository.cs
@@ -1,9 +1,12 @@
-﻿namespace Backend.Repositories
+﻿using System.Linq.Expressions;
+
+namespace Backend.Repositories
 {
     public interface IGenericRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync();
         Task<T> GetByIdAsync(object id);
+        Task<IEnumerable<T>> GetListAsync(Expression<Func<T, bool>> condition);
         void Add(T obj);
         void Update(T obj);
         void Delete(object id);
