88afef23d57b4226c5134245636f9da3c3b66b7b
tronghoa <hoa.truongtrong26@gmail.com>
2024-03-08 15:33:50 +0700

    Edit DB

--- Backend/Models/PRN231_V2Context.cs
+++ Backend/Models/PRN231_V2Context.cs
@@ -37,9 +37,10 @@ namespace Backend.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=TRONGHOA\\SQLEXPRESS;database=PRN231_V2;uid=sa;pwd=1;Trusted_Connection=True;Encrypt=False");
+                var ConnectionString = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("DefaultConnection");
+                optionsBuilder.UseSqlServer(ConnectionString);
             }
+
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
