39a3172246b17a6bfd71d4be8ed1ac0fd956ebb7
nguyen hong son <sonnh296@gmail.com>
2024-03-10 20:13:37 +0700

    login message

---
+++ Backend/DTOs/Auth/LoginResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend.DTOs.Auth {
+    public class LoginResponse {
+        public int id { get; set; }
+        public string Role { get; set; }
+        public string Token { get; set; }
+        public string Error { get; set; }
+
+
+    }
+}
