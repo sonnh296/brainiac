906f83fcc3f75a018a40f4b68a98e5bb18936641
tronghoa <hoa.truongtrong26@gmail.com>
2024-03-14 22:02:24 +0700

    add handle payment VNpay

---
+++ Frontend/Services/IVnPayService.cs
@@ -0,0 +1,10 @@
+﻿using Frontend.Models;
+
+namespace Frontend.Services
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(PaymentInformationModel model, HttpContext context, string urlCallBack);
+        PaymentResponseModel PaymentExecute(IQueryCollection collections);
+    }
+}
