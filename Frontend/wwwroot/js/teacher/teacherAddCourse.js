19969253ec0b3e63f17a5f1c87391e7a3ce1863f
Linhha <linhnhhe160458@fpt.edu.vn>
2024-03-14 18:06:38 +0700

    teacher add course draft - success

--- Frontend/wwwroot/js/teacher/teacherAddCourse.js
+++ Frontend/wwwroot/js/teacher/teacherAddCourse.js
@@ -3,17 +3,22 @@ $(document).ready(function () {
     SaveDraftCourse();
     PublishCourse();
     GetCategories();
+    
 });
 
 function SaveDraftCourse() {
     $("#btn-save-draft").click(function () {
+        var catesSeltected = getCategoryChecked();
         let teacherId = getParameter("teacherId");
         let course = GetCourseInfo();
         if (!course.title || !course.courseName || !course.price) {
             alert("Course infos are missing");
         }
+        //console.log(catesSeltected);
+
         else {
             course.status = "draft";
+            course.categories = catesSeltected;
             $.ajax({
                 headers: {
                     'Accept': '*/*',
@@ -97,13 +102,12 @@ function GetCategories() {
             for (let i = 0; i < result.length; i++) {
                 const a = `
                     <label for="${result[i].categoryId}">
-                        <input type="checkbox" id="${result[i].categoryId}" value="${result[i].categoryId}">
+                        <input type="checkbox" id="${result[i].categoryId}" name="categories[]" value="${result[i].categoryId}">
                         ${result[i].categoryName}
                     </label>`;
                 biggest += a;
             }
-            debugger;
-            console.log(biggest);
+            //console.log(biggest);
             $("#set-cate").html(biggest);
         },
         error: function (error) {
@@ -111,10 +115,28 @@ function GetCategories() {
         }
     });
 }
+
 function getParameter(param) {
     var urlVar = window.location.search.substring(1);
     var urlParam = urlVar.split('=');
     if (urlParam[0] == param) {
         return urlParam[1];
     }
+}
+
+function getCategoryChecked() {
+    var selectedCategories = [];
+    var selected = {};
+    var cateCheckbox = document.querySelectorAll('input[name="categories[]"]');
+    cateCheckbox.forEach((checkbox) => {
+        if (checkbox.checked) {
+            selected = {
+                categoryId: checkbox.getAttribute("value"),
+                categoryName: checkbox.parentElement.textContent.trim()
+            };
+            selectedCategories.push(selected);
+        }
+    });
+    //console.log(selectedCategories);
+    return selectedCategories;
 }
