a42e215c9d5696ff59e5c30ed6069cd9334cfce6
legonic2002 <93648882+legonic2002@users.noreply.github.com>
2024-03-15 13:21:17 +0700

    update font-end for sreach page

--- Backend/Repositories/IHomeRepository.cs
+++ Backend/Repositories/IHomeRepository.cs
@@ -8,13 +8,13 @@ namespace Backend.Repositories
         List<Course> GetAllCourse();
         List<CourseRatingDTO> GetPopularCourse();
         List<SortRatingDTO> GetPopularCourse2();
-        List<CourseRatingDTO> GetCourseBySearchCatePrice(string search, int cate, int rate);
+        List<CourseRatingDTO> GetCourseBySearchCatePrice(string search, int cate, int ratefrom, int rateto);
         List<CourseRatingDTO> GetCourseBySearch(string search);
         List<CourseRatingDTO> GetCourseByCate(int cate);
-        List<CourseRatingDTO> GetCourseByPrice(int rate);
+        List<CourseRatingDTO> GetCourseByPrice(int ratefrom, int rateto);
         List<CourseRatingDTO> GetCourseBySearchCate(string search, int cate);
-        List<CourseRatingDTO> GetCourseBySearchPrice(string search, int rate);
-        List<CourseRatingDTO> GetCourseByCatePrice(int cate, int rate);
+        List<CourseRatingDTO> GetCourseBySearchPrice(string search, int ratefrom, int rateto);
+        List<CourseRatingDTO> GetCourseByCatePrice(int cate, int ratefrom, int rateto);
 
     }
 }
