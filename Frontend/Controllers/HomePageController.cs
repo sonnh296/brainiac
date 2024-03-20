b6ff551400cb8e0f2aafe50dea84b9a2d7ad2e7c
legonic2002 <93648882+legonic2002@users.noreply.github.com>
2024-03-20 22:38:42 +0700

    update home page

--- Frontend/Controllers/HomePageController.cs
+++ Frontend/Controllers/HomePageController.cs
@@ -3,10 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace Frontend.Controllers {
-    public class Home2Controller : Controller {
+    public class HomePageController : Controller {
         private readonly ILogger<HomeController> _logger;
 
-        public Home2Controller(ILogger<HomeController> logger) {
+        public HomePageController(ILogger<HomeController> logger) {
             _logger = logger;
         }
 
